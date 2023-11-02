b15bf0d3979fd07f7192e55c1a0a13c285512a0f
kensing935 <77079566+kensing935@users.noreply.github.com>
2023-11-01 23:37:01 -0700

    Strings practice
    practice with Strings

---
+++ strings.playground/Contents.swift
@@ -0,0 +1,18 @@
+/** Working with strings in Swift */
+var greeting = "Hello, Strings playground"
+let day = "Monday"
+print("Today is \(day).")
+let hour = "6"
+let minutes = "15"
+let period = "PM"
+var time = hour + ":" + minutes + " " + period
+print("It is \(time).")
+print("it is \(time) on \(day).")
+let timezone = "PST"
+print("It is \(time).")
+print("It is \timezone")
+print("It is \(time).")
+print("It is \(time) on \(day) ")
+let shortDay = day.prefix(3)
+print("Today is \(shortDay).")
+print("It is \(time) on \(shortDay).")
---
+++ swifttest1/swifttest1/swifttest1App.swift
@@ -0,0 +1,17 @@
+//
+//  swifttest1App.swift
+//  swifttest1
+//
+//  Created by kensing wong on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct swifttest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
